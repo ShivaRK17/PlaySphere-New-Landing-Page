67765bdfe436cf50f3234b463d8c18ac5c3bd1e5
ShivaRK <shivarkcodes@gmail.com>
2024-06-27 04:31:24 +0530

    initial commit Co-authored-by: AAKASH751

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        cc: ['var(--font-cc)'],
+        dmmono: ['var(--font-dmmono)'],
+        lato:['var(--font-lato)'],
+        bsst:['var(--font-bsst)']
+      },
     },
   },
   plugins: [],
